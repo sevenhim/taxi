33b99dbbe5223f58e467e1637939240e94d9e1f3
sevenhim <101778766+sevenhim@users.noreply.github.com>
2022-03-26 17:56:04 +0700

    program taxi

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiConsoleApp
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "adhim";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 3.ToString();
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
